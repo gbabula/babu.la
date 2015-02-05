856fde42c4eed7b40601b2f5331af20848f384ff
Greg Babula <gbabula@gmail.com>
2015-02-04 23:23:07 -0500

    updated progress

---
+++ src/scripts/index.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * @module index
+ * @description babu.la homepage
+ * @author Greg Babula
+ *
+ */
+
+'use strict';
+
+var g5Knockout   = require('./g5-knockout').construct;
+
+/**
+ *
+ * @function onLoad
+ *
+ */
+function onLoad() {
+
+    var babulaHomeApp = g5Knockout({
+        container: document.getElementById('babula')
+    });
+
+    babulaHomeApp.init();
+
+}
+
+window.onload = onLoad;
