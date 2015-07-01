a9fca64bcbc0f1a92df33c6d0efcf045a5ce88ed
Greg Babula <greg.babula@mlb.com>
2015-07-01 16:09:27 -0400

    simplified example

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,12 +9,7 @@
 
 const g5Component = require('g5-component');
 
-/**
- *
- * @function onLoad
- *
- */
-function onLoad() {
+window.onload = () => {
 
     let infoComponent = g5Component({
         container: document.querySelector('.g5-component--info'),
@@ -23,70 +18,7 @@ function onLoad() {
         enablePolling: false
     });
 
-    // attach events before init
-    attachEvents(infoComponent);
-
     // init component
     infoComponent.init();
 
-}
-
-/**
- *
- * @function attachEvents
- * @description event usage example
- *
- */
-function attachEvents(component) {
-
-    /**
-     *
-     * @event ready
-     * @param {Object} obj
-     *
-     */
-    component.on('ready', function(obj) {
-
-        console.log('component model and viewModel have been initiated');
-
-    });
-
-    /**
-     *
-     * @event data
-     * @param {Object} data
-     *
-     */
-    component.on('data', function(data) {
-
-        console.log('component data from model');
-
-    });
-
-    /**
-     *
-     * @event data-error
-     * @param {Object} err
-     *
-     */
-    component.on('data-error', function(err) {
-
-        console.log('component model data error');
-
-    });
-
-    /**
-     *
-     * @event destroy
-     * @param {Object} obj
-     *
-     */
-    component.on('destroy', function(obj) {
-
-        console.log('component instance killed');
-
-    });
-
-}
-
-window.onload = onLoad;
+};
